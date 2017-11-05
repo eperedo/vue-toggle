ff1c2c3ce8b8d056ff6d3e618016c60bad9d26e9
Eduardo Peredo <jrperedo@gmail.com>
2017-11-05 12:18:13 -0500

    feat(Component): Added support for gradient colors

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,11 @@
 		</header>
 		<main>
 			<p>
+				<pre>
+					&#60;v-toggle id="v-t-default" v-model="toggleValue" /&#62;
+				</pre>
 				Default
-				<v-toggle id="v-t-default" v-model="toggleValue" v-on:input="getValue('toggleValue')" />
+				<v-toggle id="v-t-default" v-model="toggleValue" />
 			</p>
 			<div>
 				<header>
@@ -15,17 +18,28 @@
 					</h2>
 				</header>
 			<p>
+				<pre>
+					&#60;v-toggle id="v-t-color" v-model="colorValue"
+					active-color="rgba(0,0,0,.38)" inactive-color="peru" /&#62;
+				</pre>
 				Perú
-				<v-toggle id="v-t-color" v-model="colorValue" v-on:input="getValue('colorValue')"
+				<v-toggle id="v-t-color" v-model="colorValue"
 					active-color="rgba(0,0,0,.38)" inactive-color="peru" />
 			</p>
 			<p>
+				<pre>
+					&#60;v-toggle id="v-t-success" v-model="successValue"
+						active-color="#4caf50" inactive-color="rgba(0,0,0,.38)"	/&#62;
+				</pre>
 				Success
-				<v-toggle id="v-t-success" v-model="successValue" v-on:input="getValue('successValue')"
+				<v-toggle id="v-t-success" v-model="successValue"
 					active-color="#4caf50" inactive-color="rgba(0,0,0,.38)" />
 			</p>
 			</div>
 			<p>
+				<pre>
+					&#60;id="v-t-disabled" disabled="true" /&#62;
+				</pre>
 				Disabled:
 				<v-toggle id="v-t-disabled" disabled="true" />
 			</p>
@@ -34,9 +48,30 @@
 					<h2 class="title">Custom Size</h2>
 				</header>
 				<p>
+				<pre>
+					&#60;v-toggle v-bind:width="36" v-bind:height="22"
+						id="v-t-small" v-model="toggleSmall"	/&#62;
+				</pre>
 					Small
 					<v-toggle v-bind:width="36" v-bind:height="22"
-						id="v-t-small" v-model="toggleSmall" v-on:input="getValue('toggleSmall')" />
+						id="v-t-small" v-model="toggleSmall" />
+				</p>
+			</div>
+			<div>
+				<header>
+					<h2 class="title">Gradient Color</h2>
+				</header>
+				<p>
+					<pre>
+						&#60;v-toggle color-gradient
+						active-color="linear-gradient(to left, #b3205f, #7d1642)"
+						inactive-color="linear-gradient(to right, #20535e, #348899)"
+						id="v-t-gradient" v-model="toggleGradient" /&#62;
+					</pre>
+					<v-toggle color-gradient
+						active-color="linear-gradient(to left, #b3205f, #7d1642)"
+						inactive-color="linear-gradient(to right, #20535e, #348899)"
+						id="v-t-gradient" v-model="toggleGradient" />
 				</p>
 			</div>
 		</main>
@@ -54,10 +89,11 @@ export default {
 	name: 'app',
 	data() {
 		return {
-			successValue: true,
-			colorValue: true,
+			colorValue: false,
+			successValue: false,
+			toggleGradient: false,
+			toggleSmall: false,
 			toggleValue: true,
-			toggleSmall: true,
 		};
 	},
 	components: {
@@ -73,4 +109,12 @@ export default {
 .title {
 	text-align: center;
 }
+
+pre {
+	background-color: #f8f8f8;
+	padding: 1%;
+	max-width: 500px;
+	white-space: pre-line;
+}
+
 </style>
